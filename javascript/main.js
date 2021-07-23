cd2853e7610af8320cd6e7af5e68179245450ba8
bhavi <bhavishya20@gmail.com>
2021-07-23 14:30:44 +0530

    Update for Preview

--- javascript/main.js
+++ javascript/main.js
@@ -1,30 +1,23 @@
 const imageSelector = document.getElementById('imageSelector');
 const imageListDiv = document.getElementById('imageListDiv');
 const imagePreviewDiv = document.getElementById('imagePreviewContainer');
-let previewImage = document.getElementById('imagePreview')
+const filterBarDiv = document.getElementById("filterBar")
 
 const grayScaleButton = document.getElementById('grayScaleButton')
 const blurButton = document.getElementById('blurButton')
+const saveButton = document.getElementById('saveButton')
+const contrastButton = document.getElementById('contrastButton')
 
-let previousProperty = "";
+let previewImage = document.getElementById('imagePreview')
 
 let imageId = 0;
 
-blurButton.addEventListener('click', function() {
-    if(previewImage.classList.contains("blur")) {
-        previewImage.classList.remove("blur")
-    } else {
-        previewImage.classList.add('blur')
-    }
-})
+blurButton.hidden = true;
+contrastButton.hidden = true;
+grayScaleButton.hidden = true;
 
-grayScaleButton.addEventListener('click', function() {
-    if(previewImage.classList.contains("grayScale")) {
-        previewImage.classList.remove("grayScale")
-    } else {
-        previewImage.classList.add('grayScale')
-    }
-})
+previewImage.hidden = true;
+saveButton.hidden = true;
 
 imageSelector.addEventListener('change', (event) => {
     const fileList = event.target.files;
@@ -37,10 +30,11 @@ imageSelector.addEventListener('change', (event) => {
 
 function createImageButton(source, id) {
     let reader = new FileReader();
-    let image = document.createElement("img")
-    let button = document.createElement("button");
 
-    reader.addEventListener("change", function() {
+    const image = document.createElement("img")
+    const button = document.createElement("button");
+
+    reader.addEventListener("load", function() {
         image.src = reader.result;
         image.alt = id;
     }, false)
@@ -59,8 +53,103 @@ function createImageButton(source, id) {
     button.addEventListener('click', function () {
         previewImage.src = reader.result;
         previewImage.alt = id;
+
+        blurButton.hidden = false;
+        grayScaleButton.hidden = false;
+        contrastButton.hidden = false;
+
+        previewImage.hidden = false;
+        saveButton.hidden = false;
     })
 
     return button;
 }
 
+saveButton.addEventListener('click', function() {
+    previewImage.download = "image.png";
+    c.toBlob(function(blob) {
+        previewImage.href = URL.createObjectURL(blob);
+        previewImage.click();
+    }, "image/png");
+})
+
+contrastButton.addEventListener('click', function() {
+    if(previewImage.classList.contains("contrast")) {
+        previewImage.classList.remove("contrast")
+    } else {
+        previewImage.classList.add('contrast')
+
+
+        if(!!document.getElementById("filterRangeSlider")) {
+            let rangeSlider = document.createElement('input')
+            rangeSlider.id = "filterRangeSlider";
+            rangeSlider.type = "range";
+            rangeSlider.classList.add("form-range")
+            rangeSlider.min = "0"
+            rangeSlider.max = "100"
+            rangeSlider.step = "1"
+            rangeSlider.value = "50"
+        } else {
+            let filterSlider = document.getElementById("filterRangeSlider")
+            filterSlider.remove()
+        }
+
+        rangeSlider.addEventListener('change', function() {
+            previewImage.style.filter = rangeSlider.value
+        })
+
+        filterBarDiv.append(rangeSlider);
+    }
+})
+
+blurButton.addEventListener('click', function() {
+    if(previewImage.classList.contains("blur")) {
+        previewImage.classList.remove("blur")
+    } else {
+        previewImage.classList.add('blur')
+
+
+        let rangeSlider = document.createElement('input')
+        rangeSlider.id = "filterRangeSlider";
+        rangeSlider.type = "range";
+        rangeSlider.classList.add("form-range")
+        rangeSlider.min = "0"
+        rangeSlider.max = "100"
+        rangeSlider.step = "1"
+        rangeSlider.value = "50"
+
+        rangeSlider.addEventListener('change', function() {
+            previewImage.style.filter = rangeSlider.value
+        })
+
+        filterBarDiv.append(rangeSlider);
+    }
+})
+
+grayScaleButton.addEventListener('click', function() {
+    if(previewImage.classList.contains("grayScale")) {
+        previewImage.classList.remove("grayScale")
+    } else {
+        previewImage.classList.add('grayScale')
+
+
+        let rangeSlider = document.createElement('input')
+        rangeSlider.id = "filterRangeSlider";
+        rangeSlider.type = "range";
+        rangeSlider.classList.add("form-range")
+        rangeSlider.min = "0"
+        rangeSlider.max = "100"
+        rangeSlider.step = "1"
+        rangeSlider.value = "50"
+
+        rangeSlider.addEventListener('change', function() {
+            previewImage.style.filter = rangeSlider.value
+        })
+
+        filterBarDiv.append(rangeSlider);
+    }
+})
+
+saveButton.addEventListener('click', function() {
+
+})
